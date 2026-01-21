85e103421f7f74d4cd5fa8d19c836acfe0de0b82
YoungSeo1104 <dudtj3116@naver.com>
2026-01-21 18:48:13 +0900

    setting

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "@/assets/styles/reset.css";
+import Layout from "./layout/Layout";
+import Main from "@/pages/main/Main";
+
+function App() {
+  return (
+    <Layout>
+      <Main />
+    </Layout>
+  );
+}
+
+export default App;
