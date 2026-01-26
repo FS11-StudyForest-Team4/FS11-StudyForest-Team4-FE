dfeb92b8e55e269a11839b218db33d58c5285164
qlwlsmwoddl0813-wq <qlwlsmwoddl0813@gmail.com>
2026-01-26 15:17:15 +0900

    feat_add header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom"; 
+import logo from "../assets/images/img_logo.svg"; 
 
 const Header = () => {
-  return <div>header 테스트</div>;
+  const location = useLocation();
+
+  return (
+    <header className="main-header">
+      <div className="header-container">
+        <div className="logo">
+          <a href="/">
+            <img src={logo} alt="공부의 숲 로고" style={{ height: "60px" }} />
+          </a>
+        </div>
+
+        {location.pathname === "/" || location.pathname.includes("/study") 
+          ? <button className="btn-signup">스터디 만들기</button> 
+          : null}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
