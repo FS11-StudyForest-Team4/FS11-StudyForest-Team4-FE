75ff7680e537e70ecd0b9a31f1ea1473afece986
qlwlsmwoddl0813-wq <qlwlsmwoddl0813@gmail.com>
2026-01-27 14:46:09 +0900

    refactor/클래스 네임변경

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -10,15 +10,15 @@ const Header = () => {
     location.pathname === "/" || location.pathname.startsWith("/study");
 
   return (
-    <header className="main-header">
-      <div className="header-container">
+    <header className="mainHeader">
+      <div className="headerContainer">
         <div className="logo">
           <Link to="/">
-            <img src={logo} alt="공부의 숲 로고" style={{ height: "60px" }} />
+            <img src={logo} alt="공부의 숲 로고" />
           </Link>
         </div>
         {shouldShowCreateButton && (
-          <Link to="/study/create" className="btn-create">
+          <Link to="/study/create" className="btnCreate">
             스터디 만들기
           </Link>
         )}
