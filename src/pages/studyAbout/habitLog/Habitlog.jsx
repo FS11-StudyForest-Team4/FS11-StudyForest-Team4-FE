40ffc676149c515398761a4687760f7258c3b578
sumforest-ha <harim227@gmail.com>
2026-01-30 17:31:53 +0900

    feat:습관기록표 로직완성

--- src/pages/studyAbout/habitLog/Habitlog.jsx
+++ src/pages/studyAbout/habitLog/Habitlog.jsx
@@ -2,56 +2,74 @@ import { useEffect, useState } from 'react';
 import styles from './Habitlog.module.css';
 import { getHabitList } from '@/api/habitService';
 import { getHabitlogs } from '@/api/habitlogService';
-import { getStartOfweek } from '@/utils/getStartOfWeek';
-
+import { getStartOfweek } from '@/utils/getStartOfweek';
+import stickerEmpty from '@/assets/icons/stickers/sticker_empty.svg';
+import stiker01 from '@/assets/icons/stickers/sticker_light_green_01.svg';
+import stiker02 from '@/assets/icons/stickers/sticker_light_green_02.svg';
+import stiker03 from '@/assets/icons/stickers/sticker_light_green_03.svg';
+import stiker04 from '@/assets/icons/stickers/sticker_light_mint_04.svg';
+import stiker05 from '@/assets/icons/stickers/sticker_light_mint_05.svg';
+import stiker06 from '@/assets/icons/stickers/sticker_green_06.svg';
+import stiker07 from '@/assets/icons/stickers/sticker_blue_07.svg';
+import stiker08 from '@/assets/icons/stickers/sticker_blue_08.svg';
+import stiker09 from '@/assets/icons/stickers/sticker_blue_09.svg';
+import stiker10 from '@/assets/icons/stickers/sticker_purple_10.svg';
+import stiker11 from '@/assets/icons/stickers/sticker_purple_11.svg';
+import stiker12 from '@/assets/icons/stickers/sticker_purple_12.svg';
+import stiker13 from '@/assets/icons/stickers/sticker_yellow_13.svg';
+import stiker14 from '@/assets/icons/stickers/sticker_yellow_14.svg';
+import stiker15 from '@/assets/icons/stickers/sticker_yellow_15.svg';
+import stiker16 from '@/assets/icons/stickers/sticker_pink_16.svg';
+import stiker17 from '@/assets/icons/stickers/sticker_pink_17.svg';
+import stiker18 from '@/assets/icons/stickers/sticker_pink_18.svg';
 // 이번주 날짜 확인 및 요청 util 만들어서 설정하기
 // return  { startDate, endDate } // 월요일부터 일요일로 설정
 // 줄마다 다른 image 넣기 로직 구현
 
-
-
-function Habitlog() { 
-  
-   const studyId = '01KG6V43DV6F8YGRN8AZ6J7XVQ'
+function Habitlog() {
+  const studyId = '01KG6V43DV6F8YGRN8AZ6J7XVQ';
 
   const [habits, setHabits] = useState([]);
-  const [habitlogs, setHabitlogs] = useState([])
+  const [habitlogs, setHabitlogs] = useState([]);
   const startOfWeek = getStartOfweek();
 
-  useEffect(() => { 
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const [habiList, habitlogList] = await Promise.all([
           getHabitList(studyId),
           getHabitlogs(studyId, startOfWeek),
-        ])
-        setHabits(habiList)
-        setHabitlogs(habitlogList)
+        ]);
+        setHabits(habiList);
+        setHabitlogs(habitlogList);
       } catch (error) {
         console.error('fetchData Error:', error);
-      } 
+      }
     };
     fetchData();
   }, [studyId, startOfWeek]);
 
-
-  // const habits = [
-  //   //GET study/habits에서 데이터 받기
-  //   { id: 1, name: '미라클모닝' },
-  //   { id: 2, name: '아침먹기' },
-  //   { id: 3, name: '스트레칭' },
-  // ];
-
-  // const habitlogs = [
-  //   // 받아올 데이터 (데이터 받을때 한 주꺼만 받기 )
-  //   { habitId: 1, createdAt: '2026-01-27'},
-  //   { habitId: 2, createdAt: '2026-01-27'},
-  //   { habitId: 4, createdAt: '2026-01-27'},
-  // ];
-
   const days = ['월', '화', '수', '목', '금', '토', '일'];
- 
-
+  const rowStickers = [
+    stiker01,
+    stiker02,
+    stiker03,
+    stiker04,
+    stiker05,
+    stiker06,
+    stiker07,
+    stiker08,
+    stiker09,
+    stiker10,
+    stiker11,
+    stiker12,
+    stiker13,
+    stiker14,
+    stiker15,
+    stiker16,
+    stiker17,
+    stiker18,
+  ];
 
   const habitlogsWithWeek = habitlogs
     .filter((h) => !h.isDeleted) // 데이터 가져올때 아예 안가져오는걸로
@@ -67,9 +85,7 @@ function Habitlog() {
       acc[habitId][day] = true;
       return acc;
     }, {});
-     
 
-     
   return (
     <section className={styles['habit-list']}>
       <h2>습관기록표</h2>
@@ -83,12 +99,20 @@ function Habitlog() {
           </tr>
         </thead>
         <tbody>
-          {habits.map((habit) => (
+          {habits.map((habit, rowIndex) => (
             <tr key={habit.id}>
               <td>{habit.name}</td>
               {days.map((day) => (
                 <td key={day}>
-                  {habitlogsWithWeek[habit.id]?.[day] ? 'O' : 'X'}
+                  {habitlogsWithWeek[habit.id]?.[day] ? (
+                    <img
+                      src={rowStickers[rowIndex]}
+                      alt="습관완료"
+                      width={36}
+                    />
+                  ) : (
+                    <img src={stickerEmpty} alt="습관미완료" width={36} />
+                  )}
                 </td>
               ))}
             </tr>
