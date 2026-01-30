6dd54f0011de487463e4ca601d509200c1ea9e9a
YoungSeo1104 <dudtj3116@naver.com>
2026-01-30 15:52:22 +0900

    feat: 버튼 컴포넌트

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = ({ onClick, children }) => {
+  return <button onClick={onClick} className={styles.btn}>{children}</button>;
+};
+
+export default Button;
