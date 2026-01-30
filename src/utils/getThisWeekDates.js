d3f1aea90861a96fe4135610efdcce0a81e80a4a
sumforest-ha <harim227@gmail.com>
2026-01-30 14:55:49 +0900

    feat: getThisWeekDates util 완성

---
+++ src/utils/getThisWeekDates.js
@@ -0,0 +1,14 @@
+export function getThisWeekDates(current = new Date()) {
+  const today = new Date(current);
+  today.setHours(0, 0, 0, 0) // 시간 초기화 
+
+  const day = today.getDay(); // 오늘 요일 확인 (0 = 일, 1 = 월)
+  const diff = (day + 6) % 7; //월요일 시작으로 조정
+  const startOfWeek = new Date(today.getTime() - diff * 24 * 60 * 60 * 1000); //월요일날짜 
+
+  const weekDates = Array.from({ length: 7 }, (_, i) => {
+    const d = new Date(startOfWeek.getTime() + i * 24 * 60 * 60 * 1000);
+    return d.toISOString().slice(0, 10);
+  });
+  return weekDates;
+}
