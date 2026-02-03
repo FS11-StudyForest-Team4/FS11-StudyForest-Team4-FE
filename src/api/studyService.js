f938f2002e1f12beb483cbc35c690f1c5464e223
sumforest-ha <harim227@gmail.com>
2026-02-03 11:29:57 +0900

    feat: 스터디수정api 코드작성

--- src/api/studyService.js
+++ src/api/studyService.js
@@ -10,3 +10,12 @@ export const createStudy = async (data) => {
   }
 };
 
+// PATCH 스터디 수정하기
+export const updateStudy = async (studyId, data) => {
+  try {
+    const res = await client.patch(`/studies/${studyId}`, data);
+    return res.data;
+  } catch (error) {
+    console.log('updateStudy Error:', error.response?.data || error.message);
+  }
+};
