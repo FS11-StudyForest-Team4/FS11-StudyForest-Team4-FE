173f043ca94891ec9939d919fd0e392b7da8da34
YoungSeo1104 <dudtj3116@naver.com>
2026-02-03 17:16:43 +0900

    feat: 비밀번호 눈모양 추가

--- src/pages/studyCreate/components/Input/Input.jsx
+++ src/pages/studyCreate/components/Input/Input.jsx
@@ -1,17 +1,45 @@
+import React, { useState } from 'react';
 import styles from './Input.module.css';
+import { visible, invisible } from '@/assets/images/icons';
 
-function Input({ label, name, errorMessage, className = '', ...props }) {
+function Input({ label, name, errorMessage, className, type, ...props }) {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className={styles.inputwrapper}>
       <label htmlFor={name}>{label}</label>
 
-      <input
-        id={name}
-        name={name}
-        className={`${styles.input} ${errorMessage ? styles.error : ''} ${className}`}
-        {...props}
-      />
-      {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+      {type === 'password' ? (
+        <div className={styles.formInput}>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            id={name}
+            name={name}
+            {...props}
+            className={`${errorMessage ? styles.error : ''} ${className}`}
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className={styles.passwordBtn}
+          >
+            <img
+              src={showPassword ? visible : invisible}
+              alt="비밀번호 보이기"
+            />
+          </button>
+        </div>
+      ) : (
+        <>
+          <input
+            id={name}
+            name={name}
+            className={`${errorMessage ? styles.error : ''} ${className}`}
+            {...props}
+          />
+        </>
+      )}
+
+      {errorMessage && <p>{errorMessage}</p>}
     </div>
   );
 }
