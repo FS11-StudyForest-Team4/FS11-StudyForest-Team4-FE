01d84ca3f9f77249b67af97148ac03d6228aa124
JoohyoungJun <bmwjjh1130@gmail.com>
2026-02-04 17:20:23 +0900

    Refactor: focus 일시정지 버튼 수정

--- src/pages/focus/Focus.jsx
+++ src/pages/focus/Focus.jsx
@@ -90,8 +90,8 @@ const Focus = ({ studyId }) => {
   };
 
   const handlePause = () => {
-    // pause 버튼
-    setIsPaused((prev) => !prev); //누르면 일시정지 <-> 재개
+    // pause 버튼입니다. 
+    setIsPaused((prev) => !prev); //누르면 일시정지 <-> 재개 
     // setIsPaused(true);
   };
 
